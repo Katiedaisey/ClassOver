66d736b02b464ebf747874a769db9d6ba41d63e7
katiedaisey <katiedaisey@yahoo.com>
2017-02-28 23:49:38 -0500

    initial commit

---
+++ server.R
@@ -0,0 +1,138 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  library(class)
+  data <- faithful
+  
+  
+  # get data loaded via fileInput
+  dataSet <- reactive({
+    infile <- input$data
+    if (is.null(infile)) {
+      # User has not uploaded a file yet
+      data <- faithful
+    } else {
+      data <- read.csv(infile$datapath, row.names = 1)
+    }
+    
+    
+    
+    sample <- 1:length(data[,1])
+    sample <- sample(sample, length(data[,1]))
+    
+    
+    
+    pro <- input$trainpor
+    pro <- pro * length(data[,1])
+    pro <- round(pro)
+    sample <- sample[1:pro]
+    train <- data[sample,]
+    test <- data[-sample,]
+    
+    scale <- input$scale
+    center <- input$center
+    
+    info <- list(data = data, sample = sample, train = train, test = test, scale = scale, center = center)
+    return(info)
+  })
+  
+  
+  
+  
+  # get column names
+  datacolumns <- reactive({
+    colnames(dataSet()$data)
+  })
+  
+  # output column names for selection
+  
+  output$selectColumn1 <- renderUI({
+    selectInput("column1", "X Variable:", datacolumns(), selected = 1)
+  })
+  output$selectColumn2 <- renderUI({
+    selectInput("column2", "Y Variable:", datacolumns(), selected = 2)
+  })
+  
+  output$selectClass <- renderUI({
+    selectInput("columnclass", "Class Variable:", datacolumns(), selected = 2)
+  })
+  
+  # generate histogram depending on column selected
+  output$distPlot <- renderPlot({
+    info <- dataSet()
+    # generate bins based on input$bins from ui.R
+    data <- scale(info$data, scale = info$scale, center = info$center)
+    x <- data[, input$column1] 
+    #x <- scale(x, scale = input$scale, center = input$center)
+    
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = input$color, border = 'white', main = "Histogram of Variable X")
+  })
+  
+  output$dist2Plot <- renderPlot({
+    info <- dataSet()
+    # generate bins based on input$bins from ui.R
+    data <- scale(info$data, scale = info$scale, center = info$center)
+    x <- data[, input$column2] 
+    #x <- scale(x, scale = input$scale, center = input$center)
+    
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = input$color, border = 'white', main = "Histogram of Variable Y")
+  })
+  
+  
+  output$twodPlot <- renderPlot({
+    info <- dataSet()
+    data <- scale(info$data, scale = info$scale, center = info$center)
+    train <- data[info$sample,]
+    test <- data[-info$sample,]
+    
+    
+    
+    plot(train[,input$column1], train[,input$column2], pch = 20, col = 1, 
+         xlim = c(min(data[,input$column1]), max(data[,input$column1])), ylim = c(min(data[,input$column2]), max(data[,input$column2])),
+         xlab = input$column1, ylab = input$column2)
+    points(test[,input$column1], test[,input$column2], pch = 20, col = 2)
+  })
+  
+  output$knnPlot <- renderPlot({
+    info <- dataSet()
+    data <- info$data
+    sample <- info$sample
+    train <- info$train
+    test <- info$test
+    
+    nam <- 1:length(info$data[1,])
+    names(nam) <- colnames(info$data)
+    cl1 <- nam[input$columnclass]
+    
+    
+    cl <- data[,cl1]
+    cltrain <- cl[sample]
+    cltest <- cl[-sample]
+    data <- data[,-(cl1)]
+    
+    data <- scale(data, scale = info$scale, center = info$center)
+    train <- data[sample,]
+    test <- data[-sample,]
+    
+    knn1 <- class:::knn(train = train, test = test, cl = cltrain, k = input$k)
+    plot(1:length(cltest), knn1, pch = 20, col = cltest,
+         xlab = "Sample No", ylab = "Predicted Class")
+    
+  })
+  
+}
+)
---
+++ ui.R
@@ -0,0 +1,88 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  navbarPage("Overview of Classification Methods", 
+             
+             
+             tabPanel("Data", 
+                      
+                      # inputs
+                      # Sidebar with a slider input for number of bins 
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          # input data
+                          fileInput("data", "CSV data:"),
+                          
+                          # center and scale data
+                          "Center and Scale Data",
+                          checkboxInput("center", "Center", value = FALSE),
+                          checkboxInput("scale", "Scale", value = FALSE),
+                          
+                          # explore data
+                          htmlOutput("selectColumn1"),
+                          htmlOutput("selectColumn2"),
+                          sliderInput("trainpor", "Proportion of Data in Training Set",
+                                      min = 0,
+                                      max = 1,
+                                      value = 0.5),
+                          sliderInput("bins",
+                                      "Number of bins:",
+                                      min = 1,
+                                      max = 50,
+                                      value = 30),
+                          
+                          selectInput("color", "Choose Color:", c("darkgrey","black", "red", "green"))
+                        ),
+                        
+                        #outputs
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          #tabsetPanel(
+                          #  tabPanel("Histogram", plotOutput("distPlot")),
+                          #  tabPanel("2D Plot", plotOutput("twodPlot"))
+                          #),
+                          plotOutput("distPlot"),
+                          plotOutput("dist2Plot"),
+                          plotOutput("twodPlot")
+                          #plotOutput("twodPlot")
+                          
+                        )
+                        
+                      )
+             ), # end Input tab
+             
+             tabPanel("knn",
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          htmlOutput("selectClass"),
+                          sliderInput("k", "Number of Neighbors",
+                                      min = 1,
+                                      max = 30,
+                                      value = 1)
+                        ),
+                        mainPanel(
+                          plotOutput("knnPlot")
+                        )
+                      )
+                      
+             ), # end knn tab
+             
+             tabPanel("PCA"), 
+             tabPanel("PLSDA"), 
+             tabPanel("CART"))
+)
+)
